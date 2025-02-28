e0d9b2be2820e63763442784a6c152c6d0253415
Oliver Kuopus <141285426+oakuopus@users.noreply.github.com>
2025-02-28 14:50:49 -0700

    start

---
+++ src/app/home/page.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {useState, useEffect} from React
+
+function weatherAPI(){
+    let [weather, setWeather] = useState()
+    return(
+        <>
+            <div className="w-[80vw] h-[80vw] m-auto bg-beige border-black border-[4px]">
+
+            </div>
+        </>
+    )
+}
+
+export default weatherAPI
